8576c03ae20f346856a11aaf4541fde923466c36
maksimchub <chubmaksim@gmail.com>
2017-08-02 17:36:22 +0300

    Maks

---
+++ lessons/_html/gulpfile.js
@@ -0,0 +1,56 @@
+var gulp					= require('gulp'),
+		sass					= require('gulp-sass'),
+		autoprefixer 	= require('gulp-autoprefixer'),
+		cleanCSS    	= require('gulp-clean-css'),
+		rename       	= require('gulp-rename'),
+		browserSync  	= require('browser-sync').create(),
+		concat       	= require('gulp-concat'),
+		uglify       	= require('gulp-uglify');
+
+gulp.task('browser-sync', ['styles', 'scripts'], function() {
+		browserSync.init({
+				server: {
+						baseDir: "./app"
+				},
+				notify: false
+		});
+});
+
+gulp.task('styles', function () {
+	return gulp.src('sass/*.sass')
+	.pipe(sass({
+		includePaths: require('node-bourbon').includePaths
+	}).on('error', sass.logError))
+	.pipe(rename({suffix: '.min', prefix : ''}))
+	.pipe(autoprefixer({browsers: ['last 15 versions'], cascade: false}))
+	.pipe(cleanCSS())
+	.pipe(gulp.dest('app/css'))
+	.pipe(browserSync.stream());
+});
+
+gulp.task('scripts', function() {
+	return gulp.src([
+		'./app/libs/modernizr/modernizr.js',
+		'./app/libs/jquery/jquery-1.11.2.min.js',
+		'./app/libs/waypoints/waypoints.min.js',
+		'./app/libs/animate/animate-css.js',
+		'./app/libs/plugins-scroll/plugins-scroll.js',
+		'./app/libs/superfish/dist/js/superfish.min.js',
+		'./app/libs/owl.carousel/dist/owl.carousel.min.js',
+		'./app/libs/jQuery.mmenu/dist/jquery.mmenu.all.js',
+		'./app/libs/equalheights/equalheights.js',
+		'./app/libs/magnific-popup/dist/jquery.magnific-popup.min.js',
+		])
+		.pipe(concat('libs.js'))
+		// .pipe(uglify()) //Minify libs.js
+		.pipe(gulp.dest('./app/js/'));
+});
+
+gulp.task('watch', function () {
+	gulp.watch('sass/*.sass', ['styles']);
+	gulp.watch('app/libs/**/*.js', ['scripts']);
+	gulp.watch('app/js/*.js').on("change", browserSync.reload);
+	gulp.watch('app/*.html').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['browser-sync', 'watch']);
